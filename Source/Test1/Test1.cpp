ec9ba8208b0bf821e1677642bebc676ef0b97abb
wanickols01 <wanickols@ualr.edu>
2022-04-13 21:18:56 -0500

    Moving/DeletingFolders/C++ rebuild
    [MAINTENCE][NONCRITICAL]

---
+++ Source/Test1/Test1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Test1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Test1, "Test1" );
